e23ff320ea8a1e72497d7fab6e9a3cdfbfeaa6e5
Honza0297 <jan.beran97@gmail.com>
2023-11-14 11:49:17 +0100

    Incorporate changes from interview

--- ELFReader/ELFReader.py
+++ ELFReader/ELFReader.py
@@ -1,5 +1,5 @@
 from elftools.elf.elffile import ELFFile
-
+import argparse
 
 class ELFReader:
     """
@@ -13,16 +13,14 @@ class ELFReader:
     def read(self):
         elf_file = open(self.file, 'rb')
         self.elf_file = ELFFile(elf_file)
-        cnt = 0
-        for segment in self.elf_file.iter_segments():
-            if segment['p_type'] == 'PT_LOAD' and segment['p_flags'] & 0x1:  # Check if the segment is executable
-                print("Header {}:".format(cnt))
-                cnt += 1
-                self.print_header(segment)
-                print("")
-                if self.flag_print_data:
-                    print("Data:")
-                    print(segment.data())
+        segments = [segment for segment in self.elf_file.iter_segments() if segment['p_type'] == 'PT_LOAD' and segment['p_flags'] & 0x1]
+        for idx, segment in enumerate(segments):
+            print("Header {}:".format(idx))
+            self.print_header(segment)
+            print("")
+            if self.flag_print_data:
+                print("Data:")
+                print(segment.data())
         elf_file.close()
 
     def print_header(self, segment):
@@ -35,3 +33,12 @@ class ELFReader:
         print("->Segment Flags: {}".format(hex(segment['p_flags'])))
         print("->Alignment: {}".format(segment['p_align']))
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", type=str, help="ELF file with data")
+    parser.add_argument("-p", "--print_data", type=bool, help="Specify if print data or only headers", default="False")
+    args = parser.parse_args()
+
+    reader = ELFReader(elf_file=args.file, flag_print_data=False)
+    reader.read()
